0e5e002d8b488e073ad3e0e50b82b6b7e78f8c28
BarunMahato <161867161+BarunMahato@users.noreply.github.com>
2026-07-24 14:57:49 +0530

    Time: 0 ms (100%), Space: 19.2 MB (72.65%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ret = []
+        while matrix:
+            ret += matrix.pop(0)
+
+            if matrix and matrix[0]:
+                for rows in matrix:
+                    ret.append(rows.pop())
+            
+            if matrix:
+                ret += matrix.pop()[::-1]
+
+            if matrix and matrix[0]:
+                for rows in matrix[::-1]:
+                    ret.append(rows.pop(0))
+        return ret
+        
