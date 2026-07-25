2030fa14c2890e545755dbeed056a3d70809cb13
BarunMahato <161867161+BarunMahato@users.noreply.github.com>
2026-07-25 13:02:27 +0530

    Time: 11 ms (53.48%), Space: 21.2 MB (10.08%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,35 +1,14 @@
+from collections import deque
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        if not nums:
-            return nums
-        if nums[0] >= 0:
-            return [n * n for n in nums]
-        
-        m = len(nums) - 1
-        for index, value in enumerate(nums):
-            if value >= 0:
-                m = index
-                break
-                
-        A = nums[m:]
-        B = nums[:m][::-1]
-        B = [-1 * n for n in B]
-
-        def merge(A, B):
-            ret = []
-            a, b = 0, 0
-            while a < len(A) and b < len(B):
-                if A[a] < B[b]:
-                    ret.append(A[a])
-                    a += 1
-                else:
-                    ret.append(B[b])
-                    b += 1
-            if a < len(A):
-                ret.extend(A[a:])
-            if b < len(B):
-                ret.extend(B[b:])
-            return ret
-        merged_array =  merge(A, B)
-        return [n * n for n in merged_array]
-        
+        answer = collections.deque()
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            l, r = abs(nums[left]), abs(nums[right])
+            if l > r:
+                answer.appendleft(l * l)
+                left += 1
+            else:
+                answer.appendleft(r * r)
+                right -= 1
+        return list(answer)
