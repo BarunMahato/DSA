7113b95e191073ef464f6b1768b9c2580ed1aeb8
BarunMahato <161867161+BarunMahato@users.noreply.github.com>
2026-07-26 14:57:07 +0530

    Time: 0 ms (100%), Space: 19.4 MB (33.93%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -7,12 +7,11 @@ class Solution:
             if matrix and matrix[0]:
                 for rows in matrix:
                     ret.append(rows.pop())
-            
+
             if matrix:
                 ret += matrix.pop()[::-1]
-
+            
             if matrix and matrix[0]:
                 for rows in matrix[::-1]:
                     ret.append(rows.pop(0))
         return ret
-        
